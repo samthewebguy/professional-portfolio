65d5463b5199248389a91ebd907f98ef6a30abe7
Samthewebguy <reachsamthewebguy@gmail.com>
2026-07-10 16:59:45 +0100

    Update URL

--- src/Components/SEOManager.jsx
+++ src/Components/SEOManager.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const SITE_URL = "https://samthewebguy.com";
+const SITE_URL = "https://www.samthewebguy.com";
 
 const homepageSchema = {
   "@context": "https://schema.org",
